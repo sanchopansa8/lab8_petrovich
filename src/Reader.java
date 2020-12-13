6e1568da49a1fc554f524ae2d99300413f932309
Alexandra Rogovenko <a.rogovenko@syberry.com>
2020-12-13 15:18:00 +0300

    code was added

---
+++ src/Reader.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Reader {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        String age = scanner.nextLine();
+    }
+}
